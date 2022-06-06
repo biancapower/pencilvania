78c1bbe98143c9f50cd6f6762b92113e1bd05b59
biancapower <bianca@biancapower.com>
2022-06-06 12:50:02 +1000

    add condition enum

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,11 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {
+    last_legs: 1,
+    worn_out: 2,
+    used_once: 3,
+    fresh_and_shiny: 4
+  }
 end
