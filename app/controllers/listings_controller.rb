b19593a8094e476eea046725ecbae240e941fe7e
biancapower <bianca@biancapower.com>
2022-06-07 11:21:03 +1000

    implement authentication

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
   before_action :set_form_vars, only: %i[ new edit ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /listings or /listings.json
   def index
