0463fd691e041c20ab535e501b2fb6d41321bf36
biancapower <bianca@biancapower.com>
2022-06-14 12:29:02 +1000

    implement viewing bought and sold items for current_user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,8 +3,10 @@ class OrdersController < ApplicationController
   end
 
   def bought
+    @bought_items = current_user.bought_orders
   end
 
   def sold
+    @sold_items = current_user.sold_orders
   end
 end
